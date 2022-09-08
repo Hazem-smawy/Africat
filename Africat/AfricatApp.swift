3a3a53085569fefa49337541370962260158edfe
hazem-smawy <hazemsmawy@gmail.com>
2022-09-08 16:52:21 +0300

    Initial Commit

---
+++ Africat/AfricatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricatApp.swift
+//  Africat
+//
+//  Created by hazem smawy on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
